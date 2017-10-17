a24ca39247aae26ff0754b87fc2e5515ed26982c
Justin Balogh <justin_balogh@hotmail.com>
2017-10-17 18:58:07 -0400

    2

--- conditionalStatements/conditionalStatements/conditionalStatements.cpp
+++ conditionalStatements/conditionalStatements/conditionalStatements.cpp
@@ -2,10 +2,24 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 void main()
 {
-   
-}
+	short earnedPTS; // points the student earned this semester
+	char letterGrade;
+	cout << setprecision(4) << fixed << showpoint; // output flags, stole this from last project too :)
+	cout << "How many points did the student earn this semester?: "; cin >> earnedPTS; cout << endl;
 
+	if (earnedPTS >= 951 && earnedPTS <= 1100) {
+		letterGrade = 65;
+		cout << "The student earned a letter grade of " << letterGrade << endl;
+	}
+
+	if else(earnedPTS >= 951 && earnedPTS <= 1100) {
+		letterGrade = 65;
+		cout << "The student earned a letter grade of " << letterGrade << endl;
+	}
+}

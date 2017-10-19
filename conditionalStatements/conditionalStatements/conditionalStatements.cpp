bba2ff5928d7a0a795252983d17352d50d323dc5
Justin Balogh <justin_balogh@hotmail.com>
2017-10-18 23:36:16 -0400

    finished?

--- conditionalStatements/conditionalStatements/conditionalStatements.cpp
+++ conditionalStatements/conditionalStatements/conditionalStatements.cpp
@@ -5,11 +5,10 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
 void main()
 {
 	short earnedPTS; // points the student earned this semester
-	char letterGrade;
+	char letterGrade; // 
 	cout << setprecision(4) << fixed << showpoint; // output flags, stole this from last project too :)
 	cout << "How many points did the student earn this semester?: "; cin >> earnedPTS; cout << endl;
 
@@ -17,9 +16,23 @@ void main()
 		letterGrade = 65;
 		cout << "The student earned a letter grade of " << letterGrade << endl;
 	}
-
-	if else(earnedPTS >= 951 && earnedPTS <= 1100) {
-		letterGrade = 65;
+	else if(earnedPTS >= 801 && earnedPTS <= 950) {
+		letterGrade = 66;
 		cout << "The student earned a letter grade of " << letterGrade << endl;
+	}
+	else if(earnedPTS >= 651 && earnedPTS <= 800) {
+		letterGrade = 67;
+		cout << "The student earned a letter grade of " << letterGrade << endl;
+	}
+	else if(earnedPTS >= 501 && earnedPTS <= 650) {
+		letterGrade = 68;
+		cout << "The student earned a letter grade of " << letterGrade << endl;
+	}
+	else if(earnedPTS >= 0 && earnedPTS <= 500) {
+		letterGrade = 70;
+		cout << "The student earned a letter grade of " << letterGrade << endl;
+	}
+	else {
+		cout << "Oops looks like you entered a number outside the range." << endl;
 	}
 }

b32de1aae6525f5e5f39cba0dc9aa599151f89cd
Justin Balogh <justin_balogh@hotmail.com>
2017-10-19 00:44:36 -0400

    has the switch, conditional operator, and if-else statements

--- conditionalStatements/conditionalStatements/conditionalStatements.cpp
+++ conditionalStatements/conditionalStatements/conditionalStatements.cpp
@@ -9,12 +9,12 @@
 using namespace std;
 void main()
 {
-	int caseTester;
+	int caseTester = 1; // for the switch
 	short earnedPTS; // points the student earned this semester
 	char letterGrade; // assigned value for letter grade
 	cout << setprecision(4) << fixed << showpoint; // output flags, stole this from last project too :)
 	cout << "How many points did the student earn this semester?: "; cin >> earnedPTS; cout << endl;
-
+		// if statements
 	if (earnedPTS >= 951 && earnedPTS <= 1100) {
 		letterGrade = 65; // A
 		caseTester = 1;
@@ -36,22 +36,19 @@ void main()
 		caseTester = 1;
 	}
 	else {
-		letterGrade = 0;
-		caseTester = 0; // Incase we get a number outside the range, we still have to assign a value to letterGrade the way this is written
-
+		caseTester = 0;
 	}
-
-	switch (caseTester) // This switch statement
+		// conditional statement
+	letterGrade = (earnedPTS > 1100 || earnedPTS < 0) ? 0 : letterGrade;
+		// checks if the earnedPTS was outside the grading range of 0-1100
+		// switch statement
+	switch (caseTester) // This switch statement tests for the case of either earnedPTS is too large or not
 	{
 		case 1: 
-			cout << "The student earned a letter grade of " << letterGrade << endl;
+			cout << "The student earned a letter grade of " << letterGrade << endl; // output for the grade
 			break;
 		default:
 			cout << "Oops looks like you entered a number outside the range." << endl;
 				// If you were to enter a negative number or a number larger than 1100
 	}
-	
-
-
-
 }

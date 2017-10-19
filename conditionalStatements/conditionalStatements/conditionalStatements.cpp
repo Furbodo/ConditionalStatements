fb641efcb69f494d15873aa1379e097adeb30f9b
Justin Balogh <justin_balogh@hotmail.com>
2017-10-19 00:01:52 -0400

    final with switch statement

--- conditionalStatements/conditionalStatements/conditionalStatements.cpp
+++ conditionalStatements/conditionalStatements/conditionalStatements.cpp
@@ -1,5 +1,7 @@
 // conditionalStatements.cpp : Defines the entry point for the console application.
-//
+// Justin Balogh Oct 18 2017
+// 
+
 
 #include "stdafx.h"
 #include <iostream>
@@ -8,31 +10,53 @@ using namespace std;
 void main()
 {
 	short earnedPTS; // points the student earned this semester
-	char letterGrade; // 
+	char letterGrade; // assigned value for letter grade
 	cout << setprecision(4) << fixed << showpoint; // output flags, stole this from last project too :)
 	cout << "How many points did the student earn this semester?: "; cin >> earnedPTS; cout << endl;
 
 	if (earnedPTS >= 951 && earnedPTS <= 1100) {
-		letterGrade = 65;
-		cout << "The student earned a letter grade of " << letterGrade << endl;
+		letterGrade = 65; // A
 	}
-	else if(earnedPTS >= 801 && earnedPTS <= 950) {
-		letterGrade = 66;
-		cout << "The student earned a letter grade of " << letterGrade << endl;
+	else if (earnedPTS >= 801 && earnedPTS <= 950) {
+		letterGrade = 66; // B
 	}
 	else if(earnedPTS >= 651 && earnedPTS <= 800) {
-		letterGrade = 67;
-		cout << "The student earned a letter grade of " << letterGrade << endl;
+		letterGrade = 67; // C		
 	}
 	else if(earnedPTS >= 501 && earnedPTS <= 650) {
-		letterGrade = 68;
-		cout << "The student earned a letter grade of " << letterGrade << endl;
+		letterGrade = 68; // D
 	}
-	else if(earnedPTS >= 0 && earnedPTS <= 500) {
-		letterGrade = 70;
-		cout << "The student earned a letter grade of " << letterGrade << endl;
+	else if (earnedPTS >= 0 && earnedPTS <= 500) {
+		letterGrade = 70; // F
 	}
 	else {
-		cout << "Oops looks like you entered a number outside the range." << endl;
+		letterGrade = 0; // Incase we get a number outside the range, we still have to assign a value to letterGrade the way this is written
+
 	}
+
+	switch (letterGrade)
+	{
+		case 65: 
+			cout << "The student earned a letter grade of " << letterGrade << endl;
+			break;
+		case 66:
+			cout << "The student earned a letter grade of " << letterGrade << endl;
+			break;
+		case 67:
+			cout << "The student earned a letter grade of " << letterGrade << endl;
+			break;
+		case 68:
+			cout << "The student earned a letter grade of " << letterGrade << endl;
+			break;
+		case 70:
+			cout << "The student earned a letter grade of " << letterGrade << endl;
+			break;
+		default:
+			cout << "Oops looks like you entered a number outside the range." << endl;
+				// If you were to enter a negative number or a number larger than 1100
+	}
+	
+
+
+
 }
